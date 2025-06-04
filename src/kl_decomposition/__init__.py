4a53228a7f12225b0c44f436ef34527f44e0190e
Beremi <35385811+Beremi@users.noreply.github.com>
2025-06-04 14:41:36 +0200

    Add Newton-enhanced DE optimiser and notebook

--- src/kl_decomposition/__init__.py
+++ src/kl_decomposition/__init__.py
@@ -1,10 +1,19 @@
 """KL decomposition utilities."""
 
-from .kernel_fit import rectangle_rule, gauss_legendre_rule, fit_exp_sum, OptimiserOptions
+from .kernel_fit import (
+    rectangle_rule,
+    gauss_legendre_rule,
+    fit_exp_sum,
+    OptimiserOptions,
+    newton_with_line_search,
+    bisection_line_search,
+)
 
 __all__ = [
     "rectangle_rule",
     "gauss_legendre_rule",
     "fit_exp_sum",
     "OptimiserOptions",
+    "newton_with_line_search",
+    "bisection_line_search",
 ]
