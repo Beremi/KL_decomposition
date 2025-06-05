39791f3eeedd145028e8ef54bbfbd5b71177fee9
Beremi <35385811+Beremi@users.noreply.github.com>
2025-06-05 18:06:06 +0200

    Add Galerkin block assembly

--- src/kl_decomposition/__init__.py
+++ src/kl_decomposition/__init__.py
@@ -10,6 +10,7 @@ from .kernel_fit import (
     newton_with_line_search,
     bisection_line_search,
 )
+from .galerkin import assemble_block
 
 __all__ = [
     "rectangle_rule",
@@ -20,4 +21,5 @@ __all__ = [
     "NewtonStats",
     "newton_with_line_search",
     "bisection_line_search",
+    "assemble_block",
 ]
